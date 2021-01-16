85f0cf24652ec66833ae7413d6fba5222a7c70fe
Bayram44 <erdoganbayram035@gmail.com>
2021-01-16 12:19:20 +0200

    ilk commitim

---
+++ src/day01/IlkClass.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class IlkClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bayram'dan selamlar");
+		System.out.println  ();
+		System.out.println("naber");
+		
+	}
+
+}
